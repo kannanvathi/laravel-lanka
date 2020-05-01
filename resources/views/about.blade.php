50d051cb0b9672d77d7ad202311f60a7edb3e4c7
kannanvathi <kannanvathi@gmail.com>
2020-05-01 17:27:35 +0530

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title', 'About')
+
+@section('content')
+    <div class="jumbotron">
+        <h1 class="display-4">About</h1>
+        <p class="lead">Hi this is bharathi</p>
+        <hr class="my-4">
+        <p>UX/UI Designer</p>
+        <p class="lead">
+            <a class="btn btn-primary btn-lg" href="#" role="button">More</a>
+        </p>
+    </div>
+@endsection
